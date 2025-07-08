111a8140327177427b6d9104c0d972ac0578e39c
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-07-08 08:05:37 -0500

    aqui se guardara cada suscripcion en un archivo

---
+++ ExamenP3Ejercicio10Vallejo/Data/LogHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenP3Ejercicio10Vallejo.Data
+{
+    public static class LogHelper
+    {
+        public static async Task Agregarlog(string nombre)
+        {
+            string log = $"Se incluyo el registro de {nombre}  el {DateTime.Now:dd/MM/yyyy HH:mm}\n";
+            string path = Path.Combine(FileSystem.AppDataDirectory, "logs_Ejercicio10Vallejo.txt");
+            await File.AppendAllTextAsync(path, log);
+        }
+        public static async Task<string> LeerLogs()
+        {
+            string path = Path.Combine(FileSystem.AppDataDirectory, "logs_Ejercicio10Vallejo.txt");
+            return File.Exists(path) ? await File.ReadAllTextAsync(path) : "No hay registros disponibles xd.";
+
+        }
+    }
+}
