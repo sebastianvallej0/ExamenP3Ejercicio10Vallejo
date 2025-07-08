17effe872d8a3405a5119841b1ed0ea3a8f55872
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-07-08 07:56:16 -0500

    Manejo la conexion con sqllite

---
+++ ExamenP3Ejercicio10Vallejo/Data/AppDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamenP3Ejercicio10Vallejo.Models;
+using SQLite;   
+
+namespace ExamenP3Ejercicio10Vallejo.Data
+{
+    public class AppDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+        public AppDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Suscripcion>().Wait();
+        }
+
+
+        public Task<int> InsertSuscripcionAsync(Suscripcion suscripcion)
+        {
+            return _database.InsertAsync(suscripcion);
+        }
+
+        public Task<List<Suscripcion>> GetSuscripcionesAsync()
+        {
+            return _database.Table<Suscripcion>().ToListAsync();
+        }
+    }
+
+}
