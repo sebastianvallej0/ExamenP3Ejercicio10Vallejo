e89d68207ec9364d40093f9d5b61a78f7dd63d1b
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-07-08 08:34:18 -0500

    Valido costo impar y guardo en sql

--- ExamenP3Ejercicio10Vallejo/App.xaml.cs
+++ ExamenP3Ejercicio10Vallejo/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace ExamenP3Ejercicio10Vallejo
+﻿using System.IO;
+using ExamenP3Ejercicio10Vallejo.Data;
+namespace ExamenP3Ejercicio10Vallejo
 {
     public partial class App : Application
     {
+        public static AppDatabase Database;
         public App()
         {
             InitializeComponent();
-
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "Suscripciones_Ejercicio10Vallejo.db3");
+            Database = new AppDatabase(dbPath);
             MainPage = new AppShell();
         }
     }
---
+++ ExamenP3Ejercicio10Vallejo/ViewModels/SuscripcionViewModels.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using ExamenP3Ejercicio10Vallejo.Models;
+using ExamenP3Ejercicio10Vallejo.Data;
+
+namespace ExamenP3Ejercicio10Vallejo.ViewModels
+{
+    public class SuscripcionViewModels : INotifyPropertyChanged
+    {
+        public ObservableCollection<Suscripcion> Suscripciones { get; set; } = new();
+        public ICommand GuardarCommand { get;  }
+
+        private AppDatabase db;
+        public Suscripcion NuevaSuscripcion { get; set; } = new ();
+        public SuscripcionViewModels()
+        {
+            db = App.Database;
+            GuardarCommand= new Command(async () => await GuardarSuscripcion());
+            CargarSuscripciones();
+        }
+        private async Task GuardarSuscripcion()
+        {
+            if ((int)NuevaSuscripcion.CostoMensual % 2 == 0)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "El costo mensual debe ser impar.", "OK");
+                return;
+            }
+
+            await db.InsertSuscripcionAsync(NuevaSuscripcion);
+            await LogHelper.Agregarlog(NuevaSuscripcion.Servicio);
+            Suscripciones.Add(NuevaSuscripcion);
+            NuevaSuscripcion = new Suscripcion();
+            OnPropertyChanged(nameof(NuevaSuscripcion));
+        }
+
+        private async void CargarSuscripciones()
+        {
+            var lista = await db.GetSuscripcionesAsync();
+            Suscripciones.Clear();
+            foreach (var s in lista)
+            Suscripciones.Add(s);
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) =>
+        
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        
+        }
+
+    }
+    
+    
+
