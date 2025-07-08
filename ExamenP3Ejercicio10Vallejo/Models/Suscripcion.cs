2cccfd5242d4815f53266fb6d3cff2a2470a08a0
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-07-08 07:54:25 -0500

    creo la estructura de datos para la suscripcion

---
+++ ExamenP3Ejercicio10Vallejo/Models/Suscripcion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+namespace ExamenP3Ejercicio10Vallejo.Models
+{
+    public class Suscripcion
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string Servicio { get; set; }
+        public string CorreoAsociado { get; set; }
+        public Decimal CostoMensual { get; set; }
+        public bool RenovacionAutomatica { get; set; }
+    }
+}
+
